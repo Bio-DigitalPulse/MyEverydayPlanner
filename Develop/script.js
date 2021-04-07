df471f4ea2fc6f84170a48a801f9056e4520168f
aaron.k.keller@gmail.com <aaron.k.keller@gmail.com>
2021-04-07 00:41:05 -0400

    Adding finished code, multiple new functions, will update soon with added comments and proper README

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,53 @@
-$(document).ready(function(){
+$(document).ready(function(){   //Creates document for JS functions
 
     $(".saveBtn").on("click",function(){
-        var text =$(this).siblings(".description").val()
+        var text =$(this).siblings(".description").val()   //Save button will grab written description. And commit the variable "time" to the parent ID attribute.
         var time =$(this).parent().attr("id")
 
-        localStorage.setItem(time, text);
+        localStorage.setItem(time, text);   //Stores data in local storage
     })
+    
+    var now = moment().format("dddd, MMMM Do, YYYY, A");   //Calculates the current time for display.
+    $('#currentDay').append(now);
 
-    var now = moment().format("dddd, MMMM Do, YYYY, A");
-    $('.lead').append(now);
+    function storedData() {
+        $(".row").each(function () {
+            var textInput = $(this).attr("id");   //Retrieves the local storage data and displays within appropriate text area.
+            var savedInput = localStorage.getItem(textInput);
 
-    var timeChange = moment()
+            if (savedInput !== null) {
+                $(this).children(".description").val(savedInput)
+            }
+        })
+    }
 
-});
+    storedData();
+
+
+    function timedChange() {
+
+        var thisMoment = moment().hour(); 
+        $(".row").each(function () {
+            var scheduledMoment = parseInt($(this).attr("id").split("hour")[1]);
+
+
+
+            if (scheduledMoment < thisMoment) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (scheduledMoment === thisMoment) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+ timedChange();
+})
